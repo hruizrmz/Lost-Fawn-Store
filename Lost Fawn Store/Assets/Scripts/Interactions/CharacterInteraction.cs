a2974663f49ce19af6583a4f8a6fb772b621d469
Redthirst <Redthirst@users.noreply.github.com>
2023-02-05 00:23:56 +0200

    Added dialogue to the Front Desk scene

--- Lost Fawn Store/Assets/Scripts/Interactions/CharacterInteraction.cs
+++ Lost Fawn Store/Assets/Scripts/Interactions/CharacterInteraction.cs
@@ -2,61 +2,85 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Yarn.Unity;
 
 
 public class CharacterInteraction : MonoBehaviour
 {
-    public GameObject dialogBox;
+    private DialogueRunner dialogBox; // Yarn Dialogue Runner that handles all the lines
     public Text dialogText;
     public string dialogEmpty, dialogWrong, dialogRight;
-    public int rightItemID;
+    public int firstItemID;
+    public int secondItemID;
+    public string characterName; // The exact name of this specific character. Case-sensitive
     public bool playerInRange;
 
     private PlayerInventory inventory;
 
+    private void Start()
+    {
+        dialogBox = GameObject.Find("Dialogue System").GetComponent<DialogueRunner>();
+    }
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        // Checks whether player has no items, either of the two correct items, or an incorrect item
+        if(Input.GetKeyDown(KeyCode.Space) && playerInRange && dialogBox.IsDialogueRunning == false)
         {
-            if(dialogBox.activeInHierarchy)
+            if(inventory.item == 0)
             {
-                dialogBox.SetActive(false);
-                Time.timeScale = 1f;
-            } else {
-                dialogBox.SetActive(true);
-                Time.timeScale = 0f;
-                if (inventory.item == 0) // has no items
-                {
-                    dialogText.text = dialogEmpty;
-                    
-                } else { // has an item
-                    if (inventory.item == rightItemID) {
-                        dialogText.text = dialogRight;
-                    } else {
-                        dialogText.text = dialogWrong;
-                    }
-                }
-                inventory.item = 0;
+                dialogBox.StartDialogue(characterName + "1");
             }
+            else if(inventory.item == firstItemID)
+            {
+                dialogBox.StartDialogue(characterName + "2");
+            }
+            else if (inventory.item == secondItemID)
+            {
+                dialogBox.StartDialogue(characterName + "3");
+            }
+            else if (inventory.item > 0)
+            {
+                dialogBox.StartDialogue(characterName + "Incorrect");
+            }
+            //if(dialogBox.activeInHierarchy)
+            //{
+            //    dialogBox.SetActive(false);
+            //    Time.timeScale = 1f;
+            //} else {
+            //    dialogBox.SetActive(true);
+            //    Time.timeScale = 0f;
+            //    if (inventory.item == 0) // has no items
+            //    {
+            //        dialogText.text = dialogEmpty;
+
+            //    } else { // has an item
+            //        if (inventory.item == rightItemID) {
+            //            dialogText.text = dialogRight;
+            //        } else {
+            //            dialogText.text = dialogWrong;
+            //        }
+            //    }
+            //    inventory.item = 0;
+            //}
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        inventory = other.GetComponent<PlayerInventory>();
-        if(other.CompareTag("Player"))
+        inventory = other.gameObject.GetComponent<PlayerInventory>();
+        if(other.gameObject.CompareTag("Player"))
         {
             playerInRange = true;
         }
     }
 
-    private void OnTriggerExit2D(Collider2D other)
+    private void OnCollisionExit2D(Collision2D other)
     {
-        if(other.CompareTag("Player"))
+        if(other.gameObject.CompareTag("Player"))
         {
            playerInRange = false;
-           dialogBox.SetActive(false);
+           //dialogBox.SetActive(false);
         }
     }
 }
