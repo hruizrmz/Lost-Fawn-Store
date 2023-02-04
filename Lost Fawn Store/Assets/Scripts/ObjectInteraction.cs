2992ea5a00bb4c3ec44a1f8d192db2ab761bfbfa
paruiz01 <paruiz01@protonmail.com>
2023-02-04 04:02:31 +0200

    Added interaction between object and character

--- Lost Fawn Store/Assets/Scripts/ObjectInteraction.cs
+++ Lost Fawn Store/Assets/Scripts/ObjectInteraction.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class Sign : MonoBehaviour
+public class ObjectInteraction : MonoBehaviour
 {
     public GameObject dialogBox;
     public Text dialogText;
