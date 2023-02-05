20357ca705eb033424db2de48cfd254d234dffbd
Redthirst <Redthirst@users.noreply.github.com>
2023-02-05 10:15:17 +0200

    Fixed the "Incorrect" node not playing

--- Lost Fawn Store/Assets/Scripts/Characters/DialogueAdvancement.cs
+++ Lost Fawn Store/Assets/Scripts/Characters/DialogueAdvancement.cs
@@ -6,16 +6,32 @@ using Yarn.Unity;
 public class DialogueAdvancement : MonoBehaviour
 {
     private LineView lineView;
+    private DialogueRunner dRun;
+    private bool inputAllowed = false;
     // Start is called before the first frame update
     void Start()
     {
         lineView = GetComponent<LineView>();
+        dRun = GameObject.Find("Dialogue System").GetComponent<DialogueRunner>();
     }
 
+    public IEnumerator InputDelay()
+    {
+        yield return new WaitForSeconds(0.2f);
+        inputAllowed = true;
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (dRun.IsDialogueRunning)
+        {
+            StartCoroutine(InputDelay());
+        }
+        else if(dRun.IsDialogueRunning == false)
+        {
+            inputAllowed = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && inputAllowed)
         {
             lineView.OnContinueClicked();
         }
